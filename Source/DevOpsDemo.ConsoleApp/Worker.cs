fc68e19c410eb678eb9f0f4a4eefb84512b22b59
Matt Riedel <Matt.Riedel@ascensus.com>
2017-05-02 14:05:59 -0400

    initial commit

---
+++ Source/DevOpsDemo.ConsoleApp/Worker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevOpsDemo.ConsoleApp
+{
+    public static class Worker
+    {
+        public static int SimpleAdd(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
