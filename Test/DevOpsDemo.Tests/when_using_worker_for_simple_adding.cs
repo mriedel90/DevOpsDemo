b09e0dd1962605b4571176565334deff443b9541
Matt Riedel <Matt.Riedel@ascensus.com>
2017-06-09 09:06:02 -0400

    added test

--- Test/DevOpsDemo.Tests/when_using_worker_for_simple_adding.cs
+++ Test/DevOpsDemo.Tests/when_using_worker_for_simple_adding.cs
@@ -22,5 +22,21 @@ namespace DevOpsDemo.Tests
             Assert.IsInstanceOfType(actual, typeof(int), "Result is not int");
             Assert.AreEqual(expected, actual, "Expected result is not as expected");
         }
+
+
+        [TestMethod]
+        public void given_valid_negative_ints_returns_sum()
+        {
+            //Arrange
+            int a = -1, b = -2, expected = -3;
+
+
+            //Act
+            var actual = Worker.SimpleAdd(a, b);
+
+            //Assert
+            Assert.IsInstanceOfType(actual, typeof(int), "Result is not int");
+            Assert.AreEqual(expected, actual, "Expected result is not as expected");
+        }
     }
 }
