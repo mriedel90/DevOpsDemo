c536ea32ff0f1cf53dc7f3d443737c894c145694
Matt Riedel <Matt.Riedel@ascensus.com>
2017-06-09 11:52:02 -0400

    added new unit test project

--- Source/DevOpsDemo.ConsoleApp.UnitTests/Properties/AssemblyInfo.cs
+++ Source/DevOpsDemo.ConsoleApp.UnitTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DevOpsDemo.Tests")]
+[assembly: AssemblyTitle("DevOpsDemo.ConsoleApp.UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DevOpsDemo.Tests")]
+[assembly: AssemblyProduct("DevOpsDemo.ConsoleApp.UnitTests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b6dd89ba-cb95-45eb-8326-08defdd593eb")]
+[assembly: Guid("f1c97a5a-47af-489a-b585-ebd5102a1707")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ Source/DevOpsDemo.ConsoleApp.UnitTests/when_using_worker_for_adding.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DevOpsDemo.ConsoleApp.UnitTests
+{
+    [TestFixture]
+    public class when_using_worker_for_adding
+    {
+        [Test]
+        public void given_valid_ints_then_return_sum()
+        {
+            //Arrange
+            int a = 1, b = 2, expected = 3;
+            
+            //Act
+            var result = Worker.SimpleAdd(a, b);
+
+            //Assert
+            Assert.IsInstanceOf<int>(result, "Result is not an int");
+            Assert.AreEqual(expected, result, "Result does not match expected sum");
+        }
+    }
+}
