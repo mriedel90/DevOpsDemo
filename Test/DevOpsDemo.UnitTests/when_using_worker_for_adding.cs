13d072e1b2f16a11ace7d8513ed7e9b3c903ac43
Matt Riedel <mriedel90@gmail.com>
2017-07-13 01:01:35 -0400

    added test for negatives

--- Test/DevOpsDemo.UnitTests/when_using_worker_for_adding.cs
+++ Test/DevOpsDemo.UnitTests/when_using_worker_for_adding.cs
@@ -20,5 +20,22 @@ namespace DevOpsDemo.UnitTests
             Assert.IsInstanceOf<int>(result, "Result is not an int");
             Assert.AreEqual(expected, result, "Result does not match expected sum");
         }
+
+        [Test]
+        public void given_valid_negatives_then_return_sum()
+        {
+            //Arrange
+            int a = -1, b = -2, expected = -3;
+
+            //Act
+            var result = Worker.SimpleAdd(a, b);
+
+            //Assert
+            Assert.IsInstanceOf<int>(result, "Result is not an int");
+            Assert.AreEqual(expected, result, "Result does not match expected sum");
+        }        
+
     }
+
+
 }
